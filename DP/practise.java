e08e61e91ec6763d79c00cc815910e02592fdd06
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-14 21:51:26 +0530

    Dp

---
+++ DP/practise.java
@@ -0,0 +1,45 @@
+package DP;
+
+public class practise {
+    // climibing stair
+
+    public int ClimbingStair(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int prev1 = 1;
+        int prev2 = 1;
+                                                                                    
+        for (int i = 2; i <= n; i++) {
+            int curr = prev1 + prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+
+
+            // fibonacci number
+
+    public int fibo(int n){
+        if (n==0) {
+            return 0;
+        }
+            if (n==1) {
+                return 1;
+            
+        }
+        int prev1 = 1;
+
+        int prev2 = 0;
+        for(int i = 2;i<=n;i++){
+            int curr = prev1+prev2;
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+}
