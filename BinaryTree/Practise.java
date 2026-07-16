61f331ec38a52b5ec1611b11bb3e211eee0ad237
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-17 02:38:02 +0530

    Binary Tree add

---
+++ BinaryTree/Practise.java
@@ -0,0 +1,112 @@
+package BinaryTree;
+
+import java.util.*;
+public class Practise {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static int idx = -1;
+
+// build a binary tree
+
+    public static Node buildTree(int[] arr) {
+
+        idx++;
+
+        if (arr[idx] == -1) {
+            return null;
+        }
+
+        Node root = new Node(arr[idx]);
+
+        root.left = buildTree(arr);
+        root.right = buildTree(arr);
+
+        return root;
+    }
+
+
+    // preorder tree  pahle root left phir right
+
+    public void PreOrder(Node root){
+        if (root==null) {
+            return;
+        }
+        System.out.println(root.data);
+        PreOrder(root.left);
+        PreOrder(root.right);
+    }
+    
+       //  inorder traversal 
+    public void inorder(Node root){
+        if (root==null) {
+            return;
+        }
+        inorder(root.left);
+        System.out.println(root.data);
+        inorder(root.right);
+    }
+
+    // post order traversal
+
+    public void postorder(Node root){
+        if (root==null) {
+            return;
+        }
+        postorder(root.left);
+        postorder(root.right);
+        System.out.println(root.data);
+    }
+    
+
+// dusre tareeke se level order jisme hme answer me list send krni hoti
+
+public  List<List<Integer>> Traverse(Node root){
+
+     List<List<Integer>> ans = new ArrayList<>();
+    if (root==null) {
+        return ans;
+    }
+    Queue<Node>q = new LinkedList<>();
+    q.add(root);
+    while (!q.isEmpty()) {
+        int size = q.size();
+        List<Integer>level = new ArrayList<>();
+        for(int i = 0;i<size;i++){
+            Node curr = q.remove();
+            level.add(curr.data);
+            if (curr.left!=null) {
+                q.add(curr.left);
+            }if (curr.right!=null) {
+                q.add(curr.right);
+            }
+        }
+        ans.add(level);
+    }
+    return ans;
+}
+    
+    public static void main(String[] args) {
+
+        int[] arr = {
+                1, 2, 4, -1, -1, 5, -1, -1,
+                3, -1, 6, -1, -1
+        };
+            // agar tum print krana chahte ho to uske liye tumhe 
+            // 
+            //binrarytree bt = new binarytree()
+            // Node root = bt.buildtree(nodes)
+
+        Node root = buildTree(arr);
+    }
+}
