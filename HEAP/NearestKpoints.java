0b330a5d930c47c467ce1db93dd5041351bdae5a
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-28 10:52:13 +0530

    Weakest Soldier

--- HEAP/NearestKpoints.java
+++ HEAP/NearestKpoints.java
@@ -69,6 +69,47 @@ class NearestKpoints {
         return cost;
     }
 
+
+    //  WEAKEST ARMY SOLDIER
+
+    static class Row implements Comparable<Row>{
+        int soldiers;
+        int idx;
+        public Row(int soldiers,int idx){
+            this.soldiers = soldiers;
+            this.idx = idx;
+        }
+        @Override
+        public int compareTo(Row r2){
+            if (this.soldiers == r2.soldiers) {
+                return this.idx  - r2.idx;
+            }else{
+                return this.soldiers-r2.soldiers;
+            }
+        }
+    }
+   
+   public int[] Weakest(int[][]mat,int k){
+    PriorityQueue<Row>pq = new PriorityQueue<>();
+    for(int i = 0;i<mat.length;i++){
+        int count = 0;
+        for(int j = 0;j<mat[0].length;j++){
+            if (mat[i][j]==1) {
+                count++;
+            }
+        }
+        pq.add(new Row(count, i));
+    }
+    int[]ans = new int[k];
+    for(int i = 0;i<k;i++){
+        ans[i] = pq.remove().idx;
+    }
+    return ans;
+   }
+   
+   
+   
+   
     public static void main(String[] args) {
 
         int[][] points = {
