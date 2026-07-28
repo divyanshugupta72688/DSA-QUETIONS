f0a5eb24689c3532bb504e1541f642f429212697
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-28 15:40:10 +0530

    .

--- HEAP/NearestKpoints.java
+++ HEAP/NearestKpoints.java
@@ -1,6 +1,5 @@
 import java.util.*;
 
-
 // isme saare uwtion implement kiye gaye hai jo maine leetcode aur geeks pe solve kiye hai
 
 class NearestKpoints {
@@ -34,7 +33,7 @@ class NearestKpoints {
             int x = points[i][0];
             int y = points[i][1];
 
-            int distsq = x*x + y*y;
+            int distsq = x * x + y * y;
 
             pq.add(new Point(x, y, distsq));
 
@@ -55,91 +54,137 @@ class NearestKpoints {
         return ans;
     }
 
-// connecting n ropes
+    // connecting n ropes
 
-    public int ConnectingNRopes(int[]ropes){
-        PriorityQueue<Integer>pq = new PriorityQueue<>();
-        for(int i = 0;i<ropes.length;i++){
+    public int ConnectingNRopes(int[] ropes) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int i = 0; i < ropes.length; i++) {
             pq.add(ropes[i]);
         }
         int cost = 0;
-        while (pq.size()>1) {
+        while (pq.size() > 1) {
             int min1 = pq.remove();
             int min2 = pq.remove();
-            cost = cost+ min1+min2;
-            pq.add(min1+min2);
+            cost = cost + min1 + min2;
+            pq.add(min1 + min2);
         }
         return cost;
     }
 
+    // WEAKEST ARMY SOLDIER
 
-    //  WEAKEST ARMY SOLDIER
-
-    static class Row implements Comparable<Row>{
+    static class Row implements Comparable<Row> {
         int soldiers;
         int idx;
-        public Row(int soldiers,int idx){
+
+        public Row(int soldiers, int idx) {
             this.soldiers = soldiers;
             this.idx = idx;
         }
+
         @Override
-        public int compareTo(Row r2){
+        public int compareTo(Row r2) {
             if (this.soldiers == r2.soldiers) {
-                return this.idx  - r2.idx;
-            }else{
-                return this.soldiers-r2.soldiers;
+                return this.idx - r2.idx;
+            } else {
+                return this.soldiers - r2.soldiers;
             }
         }
     }
-   
-   public int[] Weakest(int[][]mat,int k){
-    PriorityQueue<Row>pq = new PriorityQueue<>();
-    for(int i = 0;i<mat.length;i++){
-        int count = 0;
-        for(int j = 0;j<mat[0].length;j++){
-            if (mat[i][j]==1) {
-                count++;
+
+    public int[] Weakest(int[][] mat, int k) {
+        PriorityQueue<Row> pq = new PriorityQueue<>();
+        for (int i = 0; i < mat.length; i++) {
+            int count = 0;
+            for (int j = 0; j < mat[0].length; j++) {
+                if (mat[i][j] == 1) {
+                    count++;
+                }
             }
+            pq.add(new Row(count, i));
         }
-        pq.add(new Row(count, i));
+        int[] ans = new int[k];
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.remove().idx;
+        }
+        return ans;
     }
-    int[]ans = new int[k];
-    for(int i = 0;i<k;i++){
-        ans[i] = pq.remove().idx;
-    }
-    return ans;
-   }
-   
-   
-   
-   // SLIDING WINDOW MAXIMUM 
 
-   // BRUTE FORCE APPROACH
+    // SLIDING WINDOW MAXIMUM
 
-   public int[] Maximum(int[]nums,int k){
+    // BRUTE FORCE APPROACH
+
+    public int[] Maximum(int[] nums, int k) {
         int n = nums.length;
-        int[]ans = new int[n-k+1];
-        for(int i = 0;i<=n-k;i++){
+        int[] ans = new int[n - k + 1];
+        for (int i = 0; i <= n - k; i++) {
             int max = nums[i];
-            for(int j = i;j<i+k;j++){
-                if (nums[j]>max) {
+            for (int j = i; j < i + k; j++) {
+                if (nums[j] > max) {
                     max = nums[j];
                 }
             }
             ans[i] = max;
         }
         return ans;
-   }
-    
-   
-   
-   
-   public static void main(String[] args) {
+    }
+
+    // TOP K FREQUENT ELEMENTS
+
+    static class Number implements Comparable<Number> {
+        int element;
+        int freq;
+
+        public Number(int element, int freq) {
+            this.element = element;
+            this.freq = freq;
+        }
+
+        public int compareTo(Number r2) {
+            return r2.freq - this.freq;
+        }
+    }
+
+    public int[] Top(int[] nums, int k) {
+        PriorityQueue<Number> pq = new PriorityQueue<>();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            Number number = new Number(entry.getKey(), entry.getValue());
+            pq.add(number);
+        }
+        int[] res = new int[k];
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.remove().element;
+        }
+        return res;
+    }
+
+    // kth largest element
+public int largest(int[] nums, int k) {
+
+    PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+    for (int i = 0; i < nums.length; i++) {
+
+        pq.add(nums[i]);
+
+        if (pq.size() > k) {
+            pq.remove();
+        }
+    }
+
+    return pq.peek();
+}
+
+    public static void main(String[] args) {
 
         int[][] points = {
-                {3,3},
-                {5,-1},
-                {-2,4}
+                { 3, 3 },
+                { 5, -1 },
+                { -2, 4 }
         };
 
         int k = 2;
@@ -148,11 +193,9 @@ class NearestKpoints {
 
         int[][] ans = obj.kClosest(points, k);
 
-        for(int i=0;i<ans.length;i++){
-            System.out.println(ans[i][0]+" "+ans[i][1]);
+        for (int i = 0; i < ans.length; i++) {
+            System.out.println(ans[i][0] + " " + ans[i][1]);
         }
     }
-
-
 
 }
