10f6e85ec21cc9b46d0d4982f648adca98346ae2
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-30 04:11:07 +0530

    .

--- GRAPH/Introduction.java
+++ GRAPH/Introduction.java
@@ -22,6 +22,60 @@ class Introduction {
         return -1;
     }
  
-    
-   
+    // following code shows how to store aadjacency list
+
+    public ArrayList<ArrayList<Integer>> CreateGraph(int V, int[][] edges) {
+
+    // Adjacency List banayi.
+    // Bahar wali ArrayList vertices ko store karti hai.
+    // Har vertex ke liye andar ek ArrayList hoti hai jo uske neighbours store karti hai.
+    ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
+
+    // Har vertex ke liye ek empty neighbour list banao.
+    for (int i = 0; i < V; i++) {
+        adj.add(new ArrayList<>());
+    }
+
+    // Har edge ko traverse karo.
+    for (int[] edge : edges) {
+
+        // Edge ke dono vertices nikalo.
+        int u = edge[0];
+        int v = edge[1];
+
+        // u ki neighbour list me v add karo.
+        adj.get(u).add(v);
+
+        // Agar graph undirected hai to v ki neighbour list me bhi u add karo.
+        adj.get(v).add(u);
+    }
+
+    return adj;
+}
+
+
+// jab graph ka weight diye ho graph undirected ho 
+
+static class Pair{
+    int node;
+    int weight;
+    public Pair(int node,int weight){
+        this.node = node;
+        this.weight = weight;
+    }
+}
+public  static ArrayList<ArrayList<Pair>> WeightedCreateGraph(int V,int[][]edges){
+    ArrayList<ArrayList<Pair>>res = new ArrayList<>();
+    for (int i = 0; i < V; i++) {
+        res.add(new ArrayList<>());
+    }
+    for (int[]edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        int wt = edge[2];
+        res.get(u).add(new Pair(v, wt));
+        res.get(v).add(new Pair(u, wt));
+    }
+    return res;
+}
 }
