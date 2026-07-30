13645ab98e9cd70386b2f656695c367de497ec7b
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-30 07:20:22 +0530

    .

--- GRAPH/Introduction.java
+++ GRAPH/Introduction.java
@@ -140,7 +140,50 @@ class Introduction {
     }
 }
 
+// HAS PATH EXISTS FROM SOURCE TO DESTINATION
 
+class Solution {
+
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+
+        ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+
+        boolean[] visited = new boolean[n];
+
+        return dfs(graph, visited, source, destination);
+    }
+
+    public boolean dfs(ArrayList<ArrayList<Integer>> graph,
+                       boolean[] visited,
+                       int node,
+                       int destination) {
+
+        if (node == destination)
+            return true;
+
+        visited[node] = true;
+
+        for (int neighbour : graph.get(node)) {
+
+            if (!visited[neighbour]) {
+
+                if (dfs(graph, visited, neighbour, destination))
+                    return true;
+            }
+        }
+
+        return false;
+    }
+}
 
 
 
