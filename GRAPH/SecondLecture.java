8ee797dc1c2ee09b5a836ef94241419926b4f57e
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-07-31 09:46:18 +0530

    .

--- GRAPH/SecondLecture.java
+++ GRAPH/SecondLecture.java
@@ -1,7 +1,11 @@
 import java.util.*;
 class SecondLecture{
-    // number of connected component 
-public int ConnectedComponent(int V,ArrayList<ArrayList<Integer>>graph){
+    
+    
+    // number of connected component
+    
+    
+        public int ConnectedComponent(int V,ArrayList<ArrayList<Integer>>graph){
     boolean[] visited = new boolean[V];
     int count = 0;
     for(int i = 0;i<V;i++){
@@ -20,4 +24,35 @@ public int ConnectedComponent(int V,ArrayList<ArrayList<Integer>>graph){
                 }
             }
         }
-}
+
+
+
+            // cycyle detection in graph using DFS
+
+            public boolean CycleDetection(int V,ArrayList<ArrayList<Integer>>adj){
+                boolean[]visited = new boolean[V];
+                for (int i = 0; i < V; i++) {
+                    if (!visited[i]) {
+                        if (helper(i,-1,adj,visited)) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+            public boolean helper(int  node ,int parant,ArrayList<ArrayList<Integer>>adj,boolean[]visited){
+                visited[node] = true;
+                for (int neighbour : adj.get(node)) {
+                    if (!visited[neighbour]) {
+                        if (helper(neighbour, node, adj, visited)) {
+                            return true;
+                        }
+                    }else if (neighbour != parant) {
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+    }
