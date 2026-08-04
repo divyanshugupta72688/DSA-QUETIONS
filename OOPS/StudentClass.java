75fbd6974085bd8cb3362f33b9e8167447b475a8
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-04 21:43:15 +0530

    .

--- OOPS/StudentClass.java
+++ OOPS/StudentClass.java
@@ -1,17 +1,19 @@
 public class StudentClass {
-    
-    public static class Student{
-        String name;
-        int rno;
-        double percentage;
+   
+     static class  Pen {
+    String color;
+    int tip;
+     void setcolor(String newcolor){
+        color  = newcolor;
+     } 
+     void settip(int newtip){
+        tip = newtip;
+     }
     }
     
     
     
     public static void main(String[] args) {
-        Student x = new Student();// decleartion
-        x.name = "Divyanshu";// these are the Initialization
-        x.rno = 92;
-        x.percentage = 92.55;
+   Pen p1 = new Pen();
     }
 }
