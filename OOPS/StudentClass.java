5ef16d4168961616f74245ab175f5517561760ec
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-04 18:37:03 +0530

    .

---
+++ OOPS/StudentClass.java
@@ -0,0 +1,17 @@
+public class StudentClass {
+    
+    public static class Student{
+        String name;
+        int rno;
+        double percentage;
+    }
+    
+    
+    
+    public static void main(String[] args) {
+        Student x = new Student();// decleartion
+        x.name = "Divyanshu";// these are the Initialization
+        x.rno = 92;
+        x.percentage = 92.55;
+    }
+}
