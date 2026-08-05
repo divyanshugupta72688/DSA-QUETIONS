f248b75f9f4069b68fbee52493e8e2e79e96e2d8
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-05 10:46:33 +0530

    .

---
+++ OOPS/MethodOverloading.java
@@ -0,0 +1,18 @@
+public class MethodOverloading {
+    public static void main(String[] args) {
+        Calculator cal = new Calculator();
+        System.out.println(cal.sum(1, 02));
+    }
+}
+
+class Calculator{
+    int sum(int a,int b){
+        return a+b;
+    }
+    float sum(float a, float b){
+        return a+b;
+    }
+    int sum(int a,int b,int c){
+        return a+b+c;
+    }
+}
