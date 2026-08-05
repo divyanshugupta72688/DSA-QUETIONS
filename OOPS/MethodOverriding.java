6d7f81309bfeabaeb7de675d5ecb796b704a12e0
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-05 10:55:19 +0530

    .

--- OOPS/MethodOverriding.java
+++ OOPS/MethodOverriding.java
@@ -1,3 +1,19 @@
 public class MethodOverriding {
-    
+    public static void main(String[] args) {
+        Animal d = new Dear();
+        d.eat();
+
+    }
+}
+
+class Animal{
+    void eat(){
+        System.out.println("eats anything");
+    }
+}
+
+class Dear extends Animal{
+   void eat(){
+    System.out.println("eats grass");
+   }
 }
