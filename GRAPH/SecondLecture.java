fdc4497259abc15b4f736b9e2a373684536692f2
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-06 10:52:41 +0530

    .

--- GRAPH/SecondLecture.java
+++ GRAPH/SecondLecture.java
@@ -1,102 +1,124 @@
 import java.util.*;
 import java.util.LinkedList;
-class SecondLecture{
-    
-    
+
+class SecondLecture {
+
     // number of connected component
-    
-    
-        public int ConnectedComponent(int V,ArrayList<ArrayList<Integer>>graph){
-    boolean[] visited = new boolean[V];
-    int count = 0;
-    for(int i = 0;i<V;i++){
-        if (!visited[i]) {
-            dfs(i,graph,visited);
-            count++;
+
+    public int ConnectedComponent(int V, ArrayList<ArrayList<Integer>> graph) {
+        boolean[] visited = new boolean[V];
+        int count = 0;
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                dfs(i, graph, visited);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public void dfs(int node, ArrayList<ArrayList<Integer>> graph, boolean[] visited) {
+        visited[node] = true;
+        for (int neighbour : graph.get(node)) {
+            if (!visited[neighbour]) {
+                dfs(neighbour, graph, visited);
+            }
         }
     }
-    return count;
-}
-        public void dfs(int node,ArrayList<ArrayList<Integer>>graph,boolean[] visited){
-            visited[node] = true;
-            for (int neighbour : graph.get(node)) {
-                if (!visited[neighbour]) {
-                    dfs(neighbour, graph, visited);
-                }
-            }
-        }
 
+    // cycyle detection in graph using DFS
 
-
-            // cycyle detection in graph using DFS
-
-            public boolean CycleDetection(int V,ArrayList<ArrayList<Integer>>adj){
-                boolean[]visited = new boolean[V];
-                for (int i = 0; i < V; i++) {
-                    if (!visited[i]) {
-                        if (helper(i,-1,adj,visited)) {
-                            return true;
-                        }
-                    }
-                }
-                return false;
-            }
-
-            public boolean helper(int  node ,int parant,ArrayList<ArrayList<Integer>>adj,boolean[]visited){
-                visited[node] = true;
-                for (int neighbour : adj.get(node)) {
-                    if (!visited[neighbour]) {
-                        if (helper(neighbour, node, adj, visited)) {
-                            return true;
-                        }
-                    }else if (neighbour != parant) {
-                        return true;
-                    }
-                }
-                return false;
-            }
-
-    
-            // biprate graph
-
-
-            public boolean isBipartite(int[][]graph){
-                int n = graph.length;
-                int[]color = new int[n];
-                for(int i = 0;i<color.length;i++){
-                    color[i] = -1;
-                }
-                for(int i = 0;i<n;i++){
-                    if (color[i]==-1) {
-                        if (!bfs(i,graph,color)) {
-                            return false;
-                        }
-                    }
-                }
-                return true;
-            }
-                public boolean bfs(int start,int[][]graph,int[]color){
-                    Queue<Integer>q = new LinkedList<>();
-                    q.add(start);
-                    color[start] = 1;
-                    while (!q.isEmpty()) {
-                        int node = q.remove();
-                        for(int neighbour: graph[node]){
-                            if (color[neighbour]==-1) {
-                                color[neighbour] = 1- color[node];
-                                q.add(neighbour);
-                            }else if (color[neighbour]==color[node]) {
-                                return false;
-                            }
-                        }
-                    }
+    public boolean CycleDetection(int V, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                if (helper(i, -1, adj, visited)) {
                     return true;
                 }
-    
-
-
-                // CYCYLE DETECTION USING TOPLOGICAL SORTING
-
-                
-
+            }
         }
+        return false;
+    }
+
+    public boolean helper(int node, int parant, ArrayList<ArrayList<Integer>> adj, boolean[] visited) {
+        visited[node] = true;
+        for (int neighbour : adj.get(node)) {
+            if (!visited[neighbour]) {
+                if (helper(neighbour, node, adj, visited)) {
+                    return true;
+                }
+            } else if (neighbour != parant) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    // biprate graph
+
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int[] color = new int[n];
+        for (int i = 0; i < color.length; i++) {
+            color[i] = -1;
+        }
+        for (int i = 0; i < n; i++) {
+            if (color[i] == -1) {
+                if (!bfs(i, graph, color)) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean bfs(int start, int[][] graph, int[] color) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        color[start] = 1;
+        while (!q.isEmpty()) {
+            int node = q.remove();
+            for (int neighbour : graph[node]) {
+                if (color[neighbour] == -1) {
+                    color[neighbour] = 1 - color[node];
+                    q.add(neighbour);
+                } else if (color[neighbour] == color[node]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    // CYCYLE DETECTION USING TOPLOGICAL SORTING
+
+    public static ArrayList<Integer> TopoSort(int V, ArrayList<ArrayList<Integer>> adj) {
+        Stack<Integer> s = new Stack<>();
+        boolean[] visited = new boolean[V];
+        for (int i = 0; i < V; i++) {
+            if (!visited[i]) {
+                helper(i, adj, visited, s);
+            }
+        }
+        ArrayList<Integer>ans= new ArrayList<>();
+        while (!s.isEmpty()) {
+            ans.add(s.pop());
+        }
+        return ans;
+    }
+
+    public static void helper(int node, ArrayList<ArrayList<Integer>> adj,
+                   boolean[] visited, Stack<Integer> stack) {
+
+    visited[node] = true;
+
+    for (int neighbour : adj.get(node)) {
+        if (!visited[neighbour]) {
+            helper(neighbour, adj, visited, stack);
+        }
+    }
+
+    // Push after visiting all neighbours
+    stack.push(node);
+}
+}
