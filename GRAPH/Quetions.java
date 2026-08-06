3821833783445361a5aa76b13fadbf035b855975
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-06 16:21:05 +0530

    .

--- GRAPH/Quetions.java
+++ GRAPH/Quetions.java
@@ -59,5 +59,46 @@ class Solution {
 }
 
 
+// no. of islands(200)
+
+class solution{
+    int n;
+    int m;
+    public int Islands(char[][]grid){
+        n = grid.length;
+        m = grid[0].length;
+        int count =0;
+        boolean[][]visited = new boolean[n][m];
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if (grid[i][j] == '1' && !visited[i][j]) {
+                    count++;
+                    IslandsUtil(i,j,grid,visited);
+                }
+            }
+        }
+        return count;
+    }
+
+    public void IslandsUtil(int row,int coln,char[][]grid,boolean[][]visited){
+        if (row < 0 || row >= n || coln < 0 || coln>= m) {
+            return ;
+        }
+
+        if (grid[row][coln]=='0') {
+            return;
+        }
+        if (visited[row][coln]) {
+            return ;
+        }
+         visited[row][coln] = true;
+        IslandsUtil(row-1, coln, grid, visited);
+          IslandsUtil(row+1, coln, grid, visited);
+            IslandsUtil(row, coln-1, grid, visited);
+              IslandsUtil(row, coln+1, grid, visited);
+
+    }
+}
+
 
 }
