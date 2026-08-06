db2e1cc0ab797c2945b2d752fce39a383487957f
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-06 22:46:43 +0530

    .

--- DP/FirstLecture.java
+++ DP/FirstLecture.java
@@ -87,6 +87,26 @@ public class FirstLecture {
         }
     }
 
+// CLIMBING STAIR(70)
+
+class Climbing{
+    public int ClimbingStair(int n){
+        int[]dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(n,dp);
+    }
+    public int helper(int n,int[]dp){
+        if (n==1||n==0) {
+            return 1;
+        }
+        if (dp[n]!=-1) {
+            return dp[n];
+        }
+        dp[n] = helper(n-1, dp)+helper(n-2, dp);
+        return dp[n];
+    }
+}
+
 
 
 }
