2cd47b35a6956912083cbf3aa27574a46a1288c9
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-08 15:23:13 +0530

    .

--- DP/ThirdLecture.java
+++ DP/ThirdLecture.java
@@ -48,7 +48,53 @@ public class ThirdLecture {
 
     class Minimum {
         public int MinimumPathSum(int[][] grid) {
-            return 1;
+            int m = grid.length;
+            int n = grid[0].length;
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (i == 0 && j == 0) {
+                        continue;
+                    }
+                    if (i == 0) {
+                        grid[i][j] += grid[i][j - 1];
+                    } else if (j == 0) {
+                        grid[i][j] += grid[i - 1][j];
+                    } else {
+                        grid[i][j] = grid[i][j] + Math.min(grid[i - 1][j], grid[i][j - 1]);
+                    }
+                }
+            }
+            return grid[m - 1][n - 1];
+        }
+    }
+
+    // MINIMUM PATH USING MEMOZIATION
+
+    class Memoziation {
+        public int Path(int[][] grid) {
+            int m = grid.length;
+            int n = grid[0].length;
+            int[][] dp = new int[m][n];
+            for (int i = 0; i < m; i++) {
+                for (int j = 0; j < n; j++) {
+                    dp[i][j] = -1;
+                }
+            }
+            return helper(0, 0, m, n, dp, grid);
+        }
+
+        public int helper(int row, int coln, int m, int n, int[][] dp, int[][] grid) {
+            if (row >= m || coln >= n) {
+                return Integer.MAX_VALUE;
+            }
+            if (row == m - 1 && coln == n - 1) {
+                return grid[row][coln];
+            }
+            if (dp[row][coln] != -1) {
+                return dp[row][coln];
+            }
+            return dp[row][coln] = grid[row][coln]
+                    + Math.min(helper(row + 1, coln, m, n, dp, grid), helper(row, coln + 1, m, n, dp, grid));
         }
     }
 
