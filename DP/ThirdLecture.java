e457da773ddef433746da9ad15bd37a2d6f7e3da
divyanshugupta72688 <gdivyanshu72688@gmail.com>
2026-08-08 21:54:26 +0530

    .

--- DP/ThirdLecture.java
+++ DP/ThirdLecture.java
@@ -1,3 +1,5 @@
+
+
 public class ThirdLecture {
 
     // UNIQUE PATH (62)
@@ -97,5 +99,30 @@ public class ThirdLecture {
                     + Math.min(helper(row + 1, coln, m, n, dp, grid), helper(row, coln + 1, m, n, dp, grid));
         }
     }
+
+
+// COUNT SQUARE SUBMATRICES WITH ALL ONES(1277)
+
+class Solution{
+
+    public int Problem(int[][]arr){
+        int m = arr.length;
+        int n = arr[0].length;
+        int count =0;
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                if (arr[i][j]==0) {
+                    continue;
+                }
+                if (i>0 && j>0) {
+                    arr[i][j] = arr[i][j]+Math.min(arr[i-1][j], Math.min(arr[i][j-1], arr[i-1][j-1]));
+                }
+                count += arr[i][j];
+            }
+        }
+        return count;
+    }
+}
+
 
 }
